555c2f5871f89e8f03c077ba911ea1c8b6937531
Aezrev <111585670+Aezrev@users.noreply.github.com>
2025-10-07 20:55:40 +0300

    fourth commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.use(express.static('./public'))
 
 app.get('/', (req,res) => {
     res.send("Hello world!")
-})
+});
+
+app.get("/server/ping", (req, res)=>{
+    res.send("Pong");
+});
 
 app.listen(8080)
